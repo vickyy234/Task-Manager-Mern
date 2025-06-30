04fbb17be4e0d8eb94b6578605de51386d779bec
Vignesh M <vickymayilsamy@gmail.com>
2025-06-30 15:59:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,3 +33,10 @@ app.listen(process.env.PORT, () => {
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error: ', err));
+// Server check (Ignore it)
+app.get('/', (req, res) => {
+console.log(✅ Ping received at ${new Date().toISOString()});
+(mongoose.connection.readyState === 1) ?
+res.send(<h1>Server is Running</h1> <h1 style="color:green">✅ Database connnected successfully!!</h1>) :
+res.send(<h1>Server is Running</h1> <h1 style="color:red;">❌ Database not connnected! Please check the connection string and any errors.</h1>)
+});
