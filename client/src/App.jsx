ef8630dfecbe3ef9c60d2d0e0123dce8e6e8a61d
vickyy234 <vickymayilsamy@gmail.com>
2025-06-30 21:21:51 +0530

    fix(Auth): refactor authentication logic and enhance UI elements for better user experience

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
 import Auth from './components/Auth.jsx';
 import TaskManager from './components/Task.jsx';
 
-function App() {
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const res = await axios.get('/auth/verify');
+        if (res.status === 200) {
+          console.log('User is logged in');
+          navigate('/home');
+        }
+      } catch (error) {
+        console.error(error.response?.data?.message || error.message);
+        navigate('/');
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
 
   return (
     <>
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,25 +1,4 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
-function Auth() {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const checkLoginStatus = async () => {
-            try {
-                const res = await axios.get('/auth/verify');
-                if (res.status === 200) {
-                    console.log('User is logged in');
-                    navigate('/home');
-                }
-            } catch (error) {
-                console.error(error.response?.data?.message || error.message);
-            }
-        };
-
-        checkLoginStatus();
-    }, []);
+const Auth = () => {
 
     const handleGoogleLogin = async () => {
         try {
@@ -28,7 +7,7 @@ function Auth() {
             console.error('Google login failed:', error.response?.data?.message || error.message);
         }
     }
-    
+
     const handleGithubLogin = async () => {
         try {
             window.location.href = import.meta.env.VITE_API_BASE_URL + '/auth/github';
@@ -43,14 +22,15 @@ function Auth() {
 
                 {/* Left Side */}
                 <div className="w-full md:w-[60%] flex flex-col justify-center items-center p-8 space-y-6">
-                    <h1 className="text-3xl font-bold text-gray-800">Welcome back!</h1>
+                    <h1 className="text-3xl font-bold text-black">Welcome back!</h1>
+                    <h5 className="md:font-medium text-black-300 text-center">Login to  manage your tasks efficiently</h5>
 
-                    <button className="flex items-center bg-white px-6 py-3 rounded-xl shadow-md hover:scale-105 transition duration-300 cursor-pointer" onClick={handleGoogleLogin}>
+                    <button className="flex items-center bg-white px-6 py-3 rounded-xl shadow-md hover:scale-105 hover:bg-blue-300 transition duration-300 cursor-pointer" onClick={handleGoogleLogin}>
                         <img src="./google.png" alt="Google logo" className="w-6 h-6 mr-3 cursor-pointer" />
                         <span className="text-gray-700 font-medium">Sign in with Google</span>
                     </button>
 
-                    <button className="flex items-center bg-white px-6 py-3 rounded-xl shadow-md hover:scale-105 transition duration-300 cursor-pointer" onClick={handleGithubLogin}>
+                    <button className="flex items-center bg-white px-6 py-3 rounded-xl shadow-md hover:scale-105 hover:bg-gray-400 transition duration-300 cursor-pointer" onClick={handleGithubLogin}>
                         <img src="./github.png" alt="GitHub logo" className="w-6 h-6 mr-3" />
                         <span className="text-gray-700 font-medium">Sign in with GitHub</span>
                     </button>
