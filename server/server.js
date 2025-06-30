de25cab21cfe5dc76aaeda78c3fccc660135419c
Vignesh M <vickymayilsamy@gmail.com>
2025-06-30 15:02:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,6 +39,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Server and database health check route(ignore it)
 app.get('/', (req, res) => {
+    console.log(`✅ Ping received at ${new Date().toISOString()}`);
     (mongoose.connection.readyState === 1) ?
         res.send(`<h1>Server is Running</h1> <h1 style="color:green">✅ Database connnected successfully!!</h1>`) :
         res.send(`<h1>Server is Running</h1> <h1 style="color:red;">❌ Database not connnected! Please check the connection string and any errors.</h1>`)
