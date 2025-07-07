b691e94ae630e7485a01beb953fa92360cce93d4
vickyy234 <vickymayilsamy@gmail.com>
2025-07-07 13:10:58 +0530

    fix(Navbar): enhance user profile interaction by adding dropdown menu for settings and sign out options

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,12 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { AiOutlineHome } from 'react-icons/ai';
 import { MdTaskAlt } from 'react-icons/md';
-import { CiUser } from 'react-icons/ci';
+import { CiUser, CiSettings, CiLogout } from 'react-icons/ci';
+import { IoIosCloseCircleOutline } from 'react-icons/io';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState({});
   const [showImageModal, setShowImageModal] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   useEffect(() => {
     const getUserDetails = async () => {
@@ -45,7 +47,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="flex h-fit items-center justify-between bg-white px-5 shadow-md">
-        <div className="flex items-center gap-2" onClick={handleLogOut}>
+        <div className="flex items-center gap-2">
           <img
             src="./logo.png"
             alt="App Logo"
@@ -70,35 +72,72 @@ const Navbar = () => {
             Profile
           </li>
         </ul>
-        <div
-          className="flex items-center gap-2 rounded-full hover:ring"
-          onClick={() => setShowImageModal(true)}
-        >
-          {user.image ? (
-            <img
-              src={user.image}
-              alt="User logo"
-              title={user.email}
-              className="h-12 w-12 cursor-pointer rounded-full object-cover"
-            />
-          ) : (
-            <CiUser
-              className="h-4 w-4 cursor-pointer transition duration-300 hover:scale-105"
-              alt="User logo"
-              title={user.email}
-            />
-          )}
-          <span
-            className="cursor-pointer pr-2 text-xl"
-            title={user.image ? user.username : user.email}
-          >
-            {user.username || 'User'}
-          </span>
+        <div className="flex items-center gap-2 rounded-full hover:ring">
+          <div onClick={() => setShowImageModal(true)}>
+            {user.image ? (
+              <img
+                src={user.image}
+                alt="User logo"
+                title={user.email}
+                className="h-12 w-12 cursor-pointer rounded-full object-cover"
+              />
+            ) : (
+              <CiUser
+                className="h-4 w-4 cursor-pointer transition duration-300 hover:scale-105"
+                alt="User logo"
+                title={user.email}
+              />
+            )}
+          </div>
+          <div className="user-dropdown relative">
+            <span
+              className="cursor-pointer pr-2 text-xl"
+              title={user.image ? user.username : user.email}
+              onClick={() => setShowDropdown(!showDropdown)}
+            >
+              {user.username || 'User'}
+            </span>
+
+            {showDropdown && (
+              <div className="absolute right-0 z-50 mt-3 w-35 rounded-md bg-white shadow-lg ring-1 ring-black/10">
+                <ul className="flex flex-col gap-2 p-3 text-gray-700">
+                  <li
+                    className="flex cursor-pointer items-center gap-2 px-2 py-1 text-lg hover:scale-105 hover:bg-gray-100 hover:text-black"
+                    onClick={() => {
+                      setShowDropdown(false);
+                    }}
+                  >
+                    <CiUser className="h-5 w-5" />
+                    Profile
+                  </li>
+                  <li
+                    className="flex cursor-pointer items-center gap-2 px-2 py-1 text-lg hover:scale-105 hover:bg-gray-100 hover:text-black"
+                    onClick={() => {
+                      setShowDropdown(false);
+                    }}
+                  >
+                    <CiSettings className="h-5 w-5" />
+                    Settings
+                  </li>
+                  <li
+                    className="flex cursor-pointer items-center gap-2 px-2 py-1 text-lg hover:scale-105 hover:bg-gray-100 hover:text-black"
+                    onClick={() => {
+                      setShowDropdown(false);
+                      handleLogOut();
+                    }}
+                  >
+                    <CiLogout className="h-5 w-5" />
+                    <span>Sign Out</span>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       </nav>
 
       {showImageModal && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-100">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/5 backdrop-blur-sm">
           <div className="relative">
             <img
               src={user.image}
@@ -107,11 +146,11 @@ const Navbar = () => {
               className="max-h-[80vh] min-h-[300px] max-w-[90vw] min-w-[300px] rounded-lg object-cover shadow-lg"
             />
             <button
-              className="absolute top-2 right-2 cursor-pointer rounded-full bg-white p-1 text-3xl text-black shadow hover:bg-gray-200 hover:ring"
+              className="absolute top-2 right-2 cursor-pointer text-3xl hover:scale-115 hover:text-red-500"
               title="Close"
               onClick={() => setShowImageModal(false)}
             >
-              ✕
+              <IoIosCloseCircleOutline />
             </button>
           </div>
         </div>
